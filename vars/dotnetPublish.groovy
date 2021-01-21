838ec22ff39d15089c9f80b2a4a04ba17d536cb3
Jeff Gedney <jgedney@deloitte.com>
2021-01-21 17:31:57 -0500

    fix

--- vars/dotnetPublish.groovy
+++ vars/dotnetPublish.groovy
@@ -3,7 +3,6 @@
 def call (String projectFolder,String projectName, String configuration, String framework, String outFolder){
     /*not the final code*/
           echo "Performing publish to deploy folders  ${outFolder}"
-          echo "Performing publish to deploy folders  ${outFolder}"
           bat "dotnet publish ${projectFolder}\\${projectName} --configuration ${configuration} --framework ${framework} --output ${outFolder}"
           echo "Publishing done"
     }
