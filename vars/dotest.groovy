eef928c24d58a4acb07cabf91b0fbf0380a4677b
Jeff Gedney <jgedney@deloitte.com>
2021-03-04 17:35:42 -0500

    try try again

--- vars/dotest.groovy
+++ vars/dotest.groovy
@@ -4,8 +4,9 @@ def call (){
     /*not the final code*/
           echo "Checking environmentals"
           sh "printenv | sort"
-          env.jobname = "${JOB_NAME}"
-          env.jobname = env.jobname.replaceAll('/','-') 
+          echo "get jobname"
+          env.jobname = env.JOB_NAME.replaceAll('/','-') 
+          echo "priunt env again"
           sh "printenv | sort"
           echo "done"
     }
