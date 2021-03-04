66f7071a458428f19ece3bc041529dee43c73a2f
Jeff Gedney <jgedney@deloitte.com>
2021-03-04 17:21:51 -0500

    test

--- vars/dotest.groovy
+++ vars/dotest.groovy
@@ -4,5 +4,8 @@ def call (){
     /*not the final code*/
           echo "Checking environmentals"
           sh "printenv | sort"
+          jobname = "${JOB_NAME}"
+          jobname = jobname.replaceAll('/','-') 
+          sh "printenv | sort"
           echo "done"
     }
