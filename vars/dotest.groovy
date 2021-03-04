6c932730412f4ffed868faad25be09105f86d4af
Jeff Gedney <jgedney@deloitte.com>
2021-03-04 17:38:29 -0500

    working this

--- vars/dotest.groovy
+++ vars/dotest.groovy
@@ -6,7 +6,7 @@ def call (){
           sh "printenv | sort"
           echo "get jobname"
           env.jobname = env.JOB_NAME.replaceAll('/','-') 
-          echo "print env again ${jobname}" 
+          echo "print env again ${env.jobname}" 
           sh "printenv | sort"
           echo "done"
     }
