e0a36aa0ce9fb7cf1a07e4d32451365143bdf3b7
Jeff Gedney <jgedney@deloitte.com>
2021-03-04 18:22:49 -0500

    test of prep

--- vars/dotest.groovy
+++ vars/dotest.groovy
@@ -1,20 +1,47 @@
 #!/usr/bin/env groovy
 
-def call (){
+def call (String servername){
     /*not the final code*/
         def tname = ""
-          echo "Checking environmentals"
-          sh "printenv | sort"
-          echo "1 print  ${JOB_NAME}" 
-          tname = env.JOB_NAME
-          echo "2 print ${tname}" 
-          tname = env.JOB_NAME.replaceAll('/','-')
-          echo "3 print ${tname}"
-          env.jobname = tname
-          echo "4 print  ${JOB_NAME}" 
-          echo "5 print  ${env.PemFile}" 
-          echo "5 print  ${env.jobname}" 
-          echo "6 Checking environmentals"
-          sh "printenv | sort"
-          echo "done"
+        tname = env.JOB_NAME.replaceAll('/','-')
+        env.jobname = tname
+        env.LATESTTAG= "jenkins-${tname}-latest"
+        env.registry = 'jgedney/elrr-images';
+        env.registryCredential = 'dockerhub';
+        switch(servername) 
+        { 
+            case "ELRRStorage":
+                env.PemFile = '/home/ubuntu/Private-Sub-Key.pem';
+                env.REMOTE_USER='ubuntu';
+                env.REMOTE_HOST='172.31.44.154'
+                break;
+            case "METADATA":
+                env.PemFile = '/home/ubuntu/Private-Sub-Key.pem';
+                env.REMOTE_USER='ubuntu';
+                env.REMOTE_HOST='172.31.45.79'
+                break;
+            case "ELRRTEST":
+                env.PemFile = '/home/ubuntu/Private-Sub-Key.pem';
+                env.REMOTE_USER='ubuntu';
+                env.REMOTE_HOST='172.31.39.130'
+                break;
+            case "ELRRAUTH":
+                env.PemFile = '/home/ubuntu/Private-Sub-Key.pem';
+                env.REMOTE_USER='ubuntu';
+                env.REMOTE_HOST='172.31.36.63'
+                break;
+            case "xAPI_PORTAL":
+                env.PemFile = '/home/ubuntu/Public-Sub-Key.pem';
+                env.REMOTE_USER='ubuntu';
+                env.REMOTE_HOST='172.31.2.153'
+                break;
+            case "ELRR_PORTAL":
+                env.PemFile = '/home/ubuntu/Public-Sub-Key.pem';
+                env.REMOTE_USER='ubuntu';
+                env.REMOTE_HOST='172.31.14.62'
+                break;
+            default:
+                echo "!!!!!!!!!!!!!!!!!!!!UNDEFINED SERVER ${servername} -- WILL CAUSE AN ERROR!!!!!!!!!!!!"
+                break;
+        } 
     }
