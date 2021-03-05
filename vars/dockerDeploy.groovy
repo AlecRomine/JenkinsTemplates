045206388e91041799c48035a5d0988a3410b9a7
Jeff Gedney <jgedney@deloitte.com>
2021-03-04 22:08:52 -0500

    fix alst

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -5,7 +5,7 @@ def call (){
     def gostr = "ssh -i ${env.PemFile} ${env.REMOTE_USER}@${env.REMOTE_HOST}"+' "chmod +x deploy.sh"'
     def stopstr = "ssh -i ${env.PemFile} ${env.REMOTE_USER}@${env.REMOTE_HOST}"+' "sudo snap restart docker"'
     sh stopstr 
-    //sh "scp -i ${env.PemFile} deploy.sh ${env.REMOTE_USER}@${env.REMOTE_HOST}:~/"
-    //sh gostr
-    //sh "ssh -i ${env.PemFile} ${env.REMOTE_USER}@${env.REMOTE_HOST} ./deploy.sh"
+    sh "scp -i ${env.PemFile} deploy.sh ${env.REMOTE_USER}@${env.REMOTE_HOST}:~/"
+    sh gostr
+    sh "ssh -i ${env.PemFile} ${env.REMOTE_USER}@${env.REMOTE_HOST} ./deploy.sh"
 }
