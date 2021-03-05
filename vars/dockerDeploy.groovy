632f1cd3f3bf041358a0f28ac86653338e8af364
Jeff Gedney <jgedney@deloitte.com>
2021-03-04 19:15:02 -0500

    fix

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call (String servername){
+def call (){
     /*not the final code*/
     sh 'scp -i ${env.PemFile} deploy.sh ${env.REMOTE_USER}@${env.REMOTE_HOST}:~/'
     sh 'ssh -i ${env.PemFile} ${env.REMOTE_USER}@${env.REMOTE_HOST} "chmod +x deploy.sh"'
