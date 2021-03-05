db8eb8f365cb4ccb4ef30180d2637e9b142ad1bb
Jeff Gedney <jgedney@deloitte.com>
2021-03-04 21:39:58 -0500

    fixed

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -5,5 +5,5 @@ def call (){
     def gostr = "ssh -i ${env.PemFile} ${env.REMOTE_USER}@${env.REMOTE_HOST}"+' "chmod +x deploy.sh"'
     sh "scp -i ${env.PemFile} deploy.sh ${env.REMOTE_USER}@${env.REMOTE_HOST}:~/"
     sh gostr
-    sh "ssh -i ${env.PemFile} ${env.REMOTE_USER}@${env.REMOTE_HOST} ./deploy.ssh"
+    sh "ssh -i ${env.PemFile} ${env.REMOTE_USER}@${env.REMOTE_HOST} ./deploy.sh"
 }
