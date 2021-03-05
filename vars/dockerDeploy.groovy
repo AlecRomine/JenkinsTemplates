1154c9eb2041119d840cb87f68f89ecc6389e43d
Jeff Gedney <jgedney@deloitte.com>
2021-03-04 22:00:15 -0500

    test

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -3,9 +3,9 @@
 def call (){
     /*not the final code*/
     def gostr = "ssh -i ${env.PemFile} ${env.REMOTE_USER}@${env.REMOTE_HOST}"+' "chmod +x deploy.sh"'
-    def stopstr = "ssh -i ${env.PemFile} ${env.REMOTE_USER}@${env.REMOTE_HOST}"+' "sudo systemctl restart docker"'
+    def stopstr = "ssh -i ${env.PemFile} ${env.REMOTE_USER}@${env.REMOTE_HOST}"+' "ssudo systemctl list-units --type=service"'
     sh stopstr 
-    sh "scp -i ${env.PemFile} deploy.sh ${env.REMOTE_USER}@${env.REMOTE_HOST}:~/"
-    sh gostr
-    sh "ssh -i ${env.PemFile} ${env.REMOTE_USER}@${env.REMOTE_HOST} ./deploy.sh"
+    //sh "scp -i ${env.PemFile} deploy.sh ${env.REMOTE_USER}@${env.REMOTE_HOST}:~/"
+    //sh gostr
+    //sh "ssh -i ${env.PemFile} ${env.REMOTE_USER}@${env.REMOTE_HOST} ./deploy.sh"
 }
